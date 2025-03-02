aee4804780cbe098ef59d3b13c0c0aabf7f2ecec
CesarGoto1 <cagonzalez12@espe.edu.ec>
2025-03-02 01:10:56 -0500

    Porfavor

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,7 +9,7 @@ const Register = ({ onRegisterSuccess }) => {
     const handleRegister = async (e) => {
         e.preventDefault();
 
-        const response = await fetch('https://backend-production-4e30.up.railway.app/register', {
+        const response = await fetch('${process.env.REACT_APP_API_URL}/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: email, password }),
